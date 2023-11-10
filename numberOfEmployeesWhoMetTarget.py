5a6ef8a6fa88cc49f9b5cb227576eefb922eeb2b
Ashebir Wondemeneh <83369330+Ashebir07@users.noreply.github.com>
2023-11-10 00:51:59 -0800

    Create numberOfEmployeesWhoMetTarget.py

---
+++ numberOfEmployeesWhoMetTarget.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def numberOfEmployeesWhoMetTarget(self, hours, target):
+        """
+        :type hours: List[int]
+        :type target: int
+        :rtype: int
+        """
+        count = sum(1 for h in hours if h >= target)
+
+        return count
