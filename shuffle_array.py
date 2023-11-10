1ddd2a1a6e4e0f687a431c5bba80e121effcbd67
Ashebir Wondemeneh <83369330+Ashebir07@users.noreply.github.com>
2023-11-10 00:51:00 -0800

    Create shuffle_array.py

---
+++ shuffle_array.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def shuffle(self, nums, n):
+        """
+        :type nums: List[int]
+        :type n: int
+        :rtype: List[int]
+        """
+        shuffled = []
+        for i in range(n):
+        
+            shuffled.append(nums[i])
+            shuffled.append(nums[n + i])
+
+        return shuffled
